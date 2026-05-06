82bf3ad55649577eddfafe6cb0cf05a201be6b15
Henrique Rodrigues <ctm_rd@hotmail.com>
2026-05-06 20:50:07 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,7 @@ def get_gdp_data():
 
     return gdp_df
 
-gdp_df = get_gdp_data2()
+gdp_df = get_gdp_data()
 
 # -----------------------------------------------------------------------------
 # Draw the actual page
